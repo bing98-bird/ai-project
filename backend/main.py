063c896b55272fcc9394b6d27e8e7450c002d92d
bing98-bird <usianshockwave@gmail.com>
2026-04-14 00:53:06 +0800

    update port

--- backend/main.py
+++ backend/main.py
@@ -116,4 +116,4 @@ async def clean_and_analyze_data(filename: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
